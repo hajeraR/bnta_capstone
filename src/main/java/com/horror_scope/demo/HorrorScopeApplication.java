975718590e9c5ee01735896f21b4c7f82fe3f38f
Hajera Rahman <hajerarahman@Hajeras-MacBook-Air.local>
2021-12-06 14:15:02 +0000

    initial commit

---
+++ src/main/java/com/horror_scope/demo/HorrorScopeApplication.java
@@ -0,0 +1,13 @@
+package com.horror_scope.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorrorScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorrorScopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horror_scope/demo/HorrorScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horror_scope.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorrorScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
