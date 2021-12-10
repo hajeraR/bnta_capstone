5fe868bdd2e6988511e472130c84844291b1335d
Rose <roset@roses-air.home>
2021-12-10 11:10:40 +0000

    possible css option for card

--- clientside/src/components/Calendar.js
+++ clientside/src/components/Calendar.js
@@ -13,11 +13,13 @@ export const Calendar = ({showMonth, setShownMonth, setCalendarMonth, calendar})
     return(
         <>
                 <section className="background" onClick={() => closeMonth()} ref={modalRef}>
-                    <div className="modal-wrapper" showMonth={showMonth}>
+                    {/* <div className="modal-wrapper" showMonth={showMonth}>
                         <header>
                         <p>{calendar[0].month}</p>
-                        <div className="zodiac-icon">
-                            <img src={calendar[0].zodiacIcon} alt={calendar[0].horrorScope}/>
+                        <div class="zodiac-icon">
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" viewBox="0 0 234.5 53.7"><path d="M.6 1.4L116.9 52l117-50.6" class="st0"/></svg>
+                            </span><img src={calendar[0].zodiacIcon} alt={calendar[0].horrorScope}/>
                         </div>
                         <div zodiacSign="zodiac-sign">{calendar[0].zodiacSign}</div>
                         </header>
@@ -29,6 +31,19 @@ export const Calendar = ({showMonth, setShownMonth, setCalendarMonth, calendar})
                                 <AiOutlineClose/>
                             </IconContext.Provider>
                         </button>
+                    </div> */}
+                    <div class="card">
+                    <header>
+                        <div>{calendar[0].month}</div>
+                        <div class="zodiacIcon">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" viewBox="0 0 234.5 53.7"><style></style><path d="M.6 1.4L116.9 52l117-50.6" class="st0"/></svg>
+                        </span>Logo</div>
+                        <div class="zodiac-sign">{calendar[0].zodiacSign}</div>
+                    </header>
+                    <div class="horrorscope">
+                        <p>{calendar[0].horrorScope}</p>
+                    </div>
                     </div>
                 </section>
         </>
