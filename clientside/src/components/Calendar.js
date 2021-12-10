ab5575fc250c1afdb4cfcbafd94eccc8f4630b99
Rose <roset@roses-air.home>
2021-12-10 10:43:14 +0000

    changes to card shape. Something wrong with npm start

--- clientside/src/components/Calendar.js
+++ clientside/src/components/Calendar.js
@@ -1,5 +1,7 @@
 import React, {useRef} from "react";
+import {AiOutlineClose} from 'react-icons/ai';
 import './Calendar.css';
+import {IconContext} from 'react-icons';
 
 export const Calendar = ({showMonth, setShownMonth, setCalendarMonth, calendar}) => {
     const modalRef = useRef();
@@ -12,8 +14,21 @@ export const Calendar = ({showMonth, setShownMonth, setCalendarMonth, calendar})
         <>
                 <section className="background" onClick={() => closeMonth()} ref={modalRef}>
                     <div className="modal-wrapper" showMonth={showMonth}>
-                    <h2>{calendar[0].horrorScope}</h2>
-                    <button onClick={() => closeMonth()} className="close"></button>
+                        <header>
+                        <p>{calendar[0].month}</p>
+                        <div className="zodiac-icon">
+                            <img src={calendar[0].zodiacIcon} alt={calendar[0].horrorScope}/>
+                        </div>
+                        <div zodiacSign="zodiac-sign">{calendar[0].zodiacSign}</div>
+                        </header>
+                        <div className="horrorscope">
+                        <h2>{calendar[0].horrorScope}</h2>
+                        </div>
+                        <button onClick={() => closeMonth()} className="close">
+                            <IconContext.Provider value = {{className: "close-icon"}}>
+                                <AiOutlineClose/>
+                            </IconContext.Provider>
+                        </button>
                     </div>
                 </section>
         </>
