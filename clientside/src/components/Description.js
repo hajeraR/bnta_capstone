12cb8be555a5ffc68359822c57e0947ad3b130ef
Hajera Rahman <hajerarahman@Hajeras-MacBook-Air.local>
2021-12-13 15:22:16 +0000

    got link between descriptions and images

--- clientside/src/components/Description.js
+++ clientside/src/components/Description.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { useState } from "react";
 import "../styles/description.css"
 
- export const Description = ({description}) => (
+ export const Description = ({description}) => {
 
+    const [deathShown, setDeathShown] = useState(false);
+
+
+    return (
+
+    
     <div className="zodiac_description">
        <div className="bannerPlaceholder">Banner Placeholder</div>
        <div className="zodiac_sign">
@@ -18,7 +25,7 @@ import "../styles/description.css"
            <p className="zodiac_negative_match">{description.negativeMatch}</p>
          </div>
          <div className="sun">
-        <p className="zodiac_positive_match">{description.positiveMatch}</p> 
+            <p className="zodiac_positive_match">{description.positiveMatch}</p> 
         </div> 
         <p className="zodiac_death_prediction">{description.deathPrediction}</p>
         <p className="zodiac_romance">{description.romance}</p>
@@ -26,7 +33,7 @@ import "../styles/description.css"
         <p className="zodiac_money">{description.money}</p>
         <p className="zodiac_career">{description.career}</p>    
      </div>
-    
-    
     )
+    
+ }
 
