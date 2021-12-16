b53545593df971cc2111e8d4ff5db1a01a79c2c5
Darania Muralitharan <90543261+DanaMurali@users.noreply.github.com>
2021-12-16 22:17:27 +0000

    spellinf and grammar fixes

--- serverSide/target/classes/db/migration/V4__addMonthlyData.sql
+++ serverSide/target/classes/db/migration/V4__addMonthlyData.sql
@@ -8,16 +8,16 @@ INSERT INTO monthly_horrorscopes (zodiacSign, months, horrorScope)
 VALUES ('Sagittarius','DECEMBER','Everybody knows that everybody dies. But not every day. Not today. Some days are special. Some days are so, so blessed. Some days, nobody dies at all. One path leads to despair and utter hopelessness. The other, to total extinction. Do you feel blessed?');
 
 INSERT INTO monthly_horrorscopes (zodiacSign, months, horrorScope)
-VALUES ('Gemini','DECEMBER','So now it’’s clear, the very powerful and the very stupid have one thing in common: they don’’t alter their views to fit the facts; they alter the facts to fit their views.');
+VALUES ('Gemini','DECEMBER','So now it''s clear, the very powerful and the very stupid have one thing in common: they don''t alter their views to fit the facts; they alter the facts to fit their views.');
 
 INSERT INTO monthly_horrorscopes (zodiacSign, months, horrorScope)
-VALUES ('Capricorn','DECEMBER','This month you may find yourself traversing through a wormhole in space & time. Don’’t blink. Don’’t even blink. Blink and you’re dead. Don’’t turn your back. Don’’t look away. And don’’t blink.');
+VALUES ('Capricorn','DECEMBER','This month you may find yourself traversing through a wormhole in space & time. Don''t blink. Don''t even blink. Blink and you’re dead. Don''t turn your back. Don''t look away. And don''t blink.');
 
 INSERT INTO monthly_horrorscopes (zodiacSign, months, horrorScope)
-VALUES ('Cancer','DECEMBER','Do not pity the dead, Cancer. Pity the living, and, above all those who live without love. And of course, if you are in love. Watch your back, people aren’’t who you think they are.');
+VALUES ('Cancer','DECEMBER','Do not pity the dead, Cancer. Pity the living, and, above all those who live without love. And of course, if you are in love. Watch your back, people aren''t who you think they are.');
 
 INSERT INTO monthly_horrorscopes (zodiacSign, months, horrorScope)
-VALUES ('Taurus','DECEMBER','Winter is coming! We know what’s coming. Avoid the north of the wall this month at any cost, not that it’’ll do you any good. This is most probably the best time to update your will.');
+VALUES ('Taurus','DECEMBER','Winter is coming! We know what’s coming. Avoid the north of the wall this month at any cost, not that it''ll do you any good. This is most probably the best time to update your will.');
 
 INSERT INTO monthly_horrorscopes (zodiacSign, months, horrorScope)
 VALUES ('Scorpio','DECEMBER','The world is indeed full of peril, and in it there are many dark places; there is not much that is fair, in all lands, love is now mingled with grief, grow greater it shall.');
@@ -26,7 +26,7 @@ INSERT INTO monthly_horrorscopes (zodiacSign, months, horrorScope)
 VALUES ('Leo','DECEMBER','Temper, temper. No one would ever dream of challenging you. Well, as far as brains go, you got the lion''s share. But when it comes to brute strength, I''m afraid you''re at the shallow end of the gene pool. Be warned of natural selection.');
 
 INSERT INTO monthly_horrorscopes (zodiacSign, months, horrorScope)
-VALUES ('Pisces','DECEMBER','We will hurt you for this. A day this month will come when you think you’’re safe and happy, and your joy will turn to ashes in your mouth, and you’’ll know the debt is paid.');
+VALUES ('Pisces','DECEMBER','We will hurt you for this. A day this month will come when you think you’’re safe and happy, and your joy will turn to ashes in your mouth, and you''ll know the debt is paid.');
 
 INSERT INTO monthly_horrorscopes (zodiacSign, months, horrorScope)
-VALUES ('Libra','DECEMBER','You’’re quite good at spending money, but a lifetime of outrageous wealth hasn’t taught you much about managing it. Your words will disappear. Your house will disappear. Your name will disappear. All memories of you will disappear.');
+VALUES ('Libra','DECEMBER','You''re quite good at spending money, but a lifetime of outrageous wealth hasn’t taught you much about managing it. Your words will disappear. Your house will disappear. Your name will disappear. All memories of you will disappear.');
